014fc918192cd96fadc4fd72bc2447e6626de308
minsoeaung <minsoeaung2001@gmail.com>
2023-07-14 23:34:46 +0630

    Add missing migration files

---
+++ migrations/20230714165039_DepartmentCourseInstructorOffice_CourseWithJustNullalbeDepartmentId.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ContosoPizza.migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentCourseInstructorOffice_CourseWithJustNullalbeDepartmentId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Course",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Course",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Instructors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HireDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseInstructor",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "integer", nullable: false),
+                    InstructorsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseInstructor", x => new { x.CoursesId, x.InstructorsId });
+                    table.ForeignKey(
+                        name: "FK_CourseInstructor_Course_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseInstructor_Instructors_InstructorsId",
+                        column: x => x.InstructorsId,
+                        principalTable: "Instructors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Budget = table.Column<decimal>(type: "money", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    InstructorId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Departments_Instructors_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructors",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OfficeAssignments",
+                columns: table => new
+                {
+                    InstructorId = table.Column<int>(type: "integer", nullable: false),
+                    Location = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfficeAssignments", x => x.InstructorId);
+                    table.ForeignKey(
+                        name: "FK_OfficeAssignments_Instructors_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_DepartmentId",
+                table: "Course",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseInstructor_InstructorsId",
+                table: "CourseInstructor",
+                column: "InstructorsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_InstructorId",
+                table: "Departments",
+                column: "InstructorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Departments_DepartmentId",
+                table: "Course",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Departments_DepartmentId",
+                table: "Course");
+
+            migrationBuilder.DropTable(
+                name: "CourseInstructor");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "OfficeAssignments");
+
+            migrationBuilder.DropTable(
+                name: "Instructors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_DepartmentId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Course");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Course",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ migrations/20230714170013_DepartmentCourseInstructorOffice_CourseWithNonNullableDepartmentIdForeignKey.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentCourseInstructorOffice_CourseWithNonNullableDepartmentIdForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Departments_DepartmentId",
+                table: "Course");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Course",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Departments_DepartmentId",
+                table: "Course",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Departments_DepartmentId",
+                table: "Course");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Course",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Departments_DepartmentId",
+                table: "Course",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+    }
+}
